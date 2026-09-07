93a9eac3416b3e977aa3a98a944f61eae3a01c09
2535ShekharMourya <113845874+2535ShekharMourya@users.noreply.github.com>
2026-09-07 18:11:07 +0530

    reverse, cycle, remove_from_first in linkedList

--- src/main/kotlin/problemSolving/LinkedList/MainLinkedListJ.java
+++ src/main/kotlin/problemSolving/LinkedList/MainLinkedListJ.java
@@ -21,18 +21,34 @@ class ListNode {
 public class MainLinkedListJ {
     public static void main(String[] args) {
         ListNode head = createLinkedList(10);
+        ListNode resNode = removeFirstNode(head);
+        while (resNode !=null){
+            System.out.println("--> "+resNode.val);
+            resNode = resNode.next;
+        }
 
-      /*  // Traverse LinkedList
+    }
+
+  /*  public static ListNode removeFromLast(ListNode head){
         ListNode curr = head;
-        while(curr !=null){
-            System.out.print(curr.val + " --> ");
-            curr = curr.next;
-        }*/
 
-        ListNode resNode = solveLinkedListProblem(head);
-        System.out.println( "this is my middle node value " + resNode.val);
+    }*/
+    public static ListNode removeFirstNode(ListNode head){
+        return  head.next.next.next.next.next; // removing from beginning  O(1) time
+    }
 
 
+    public static ListNode reverseLinkedList(ListNode head){
+        ListNode current = head;
+        ListNode prev = null;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
     }
 
     public static ListNode solveLinkedListProblem(ListNode head){
@@ -44,6 +60,17 @@ public class MainLinkedListJ {
         }
 
         return slow;
+    }
+    public static boolean cycleInLinkedList(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next !=null && fast.next.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow)return  true;
+        }
+
+        return false;
     }
 
     public static ListNode createLinkedList(int n) {
@@ -57,6 +84,7 @@ public class MainLinkedListJ {
             current.next = new ListNode(i);
             current = current.next;
         }
+      // current.next = head.next.next;           it is for cycle detection problem
 
         return head;
     }
